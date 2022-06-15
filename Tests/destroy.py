4c06040d47880a2105372d61bfddea7dcab8a357
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-15 23:53:34 +0530

    Create destroy.py

---
+++ Tests/destroy.py
@@ -0,0 +1,21 @@
+import time, os, sys
+from utils import *
+from var import *
+import attackslib
+
+#Get the service principal and secret values
+principal= sys.argv[1]
+password = sys.argv[2]
+
+print("Connecting to Azure CLI")
+az_id = az_login(principal,password,tenantid)
+if az_id:
+    try:            
+        print("Destroying the Infra.")  
+        for service in del_cfg:
+            az_get_cmd_op(service) 
+            time.sleep(7)
+    except BaseException:
+        logging.exception("An exception was thrown under Destroy!")
+else:
+    print("Error: Unable to connect to Azure CLI")
