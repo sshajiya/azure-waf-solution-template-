5e435e61d1c656310807a699f8bed2c761c4b66f
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-15 20:50:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,6 @@ password = sys.argv[2]
 print("Connecting to Azure CLI")
 az_id = az_login(principal,password,tenantid)
 if az_id:             
-    try:
         #Get the instance details from Virtual machine scaleset
         inst_info=az_get_cmd_op(get_vmss)
         vmss_ip_lst=get_ip(inst_info)
@@ -26,22 +25,21 @@ if az_id:
         print("VMSS Instance Details:", vmss_ip_lst, vmss_port_list)        
         
         if NAP_TEST:
-            turn_instance_state(str(vmss_port_list[1])[-1],"stop",vmssName,resource_group)        
-            
-            #NAP Functional Test
-            print("NGINX Functionality Test with Static Page, Dynamic Page, mallicious attacks")
-            if vfy_nginx(vmss_ip_lst[0],chk_def):
-                print("NGINX Static Page Verification is Completed")
-            else:
-                print("ERROR:  NGINX Static Page Verification is Failed!!!")
-            
-            ssh_id=ssh_connect(vmss_ip_lst[0],vmss_port_list[0],username,vm_password)
-            with SCPClient(ssh_id.get_transport()) as scp:  scp.put('nginx_conf_nap.conf','nginx.conf')
-            for cmd in [command_lst,command_lst2]:
-                exec_shell_cmd(ssh_id,cmd)
-                time.sleep(10)
-            #ssh_id.close() 
             try:
+                turn_instance_state(str(vmss_port_list[1])[-1],"stop",vmssName,resource_group)
+                #NAP Functional Test
+                print("NGINX Functionality Test with Static Page, Dynamic Page, mallicious attacks")
+                if vfy_nginx(vmss_ip_lst[0],chk_def):
+                    print("NGINX Static Page Verification is Completed")
+                else:
+                    print("ERROR:  NGINX Static Page Verification is Failed!!!")
+
+                ssh_id=ssh_connect(vmss_ip_lst[0],vmss_port_list[0],username,vm_password)
+                with SCPClient(ssh_id.get_transport()) as scp:  scp.put('nginx_conf_nap.conf','nginx.conf')
+                for cmd in [command_lst,command_lst2]:
+                    exec_shell_cmd(ssh_id,cmd)
+                    time.sleep(10)
+                #ssh_id.close() 
                 if vfy_nginx(vmss_ip_lst[0],chk_str):
                     print("Nginx App Protect dynamic page verification with Arcadia Application is Sucessfull!!!")
                     print("NAP  Functionality Test with Invalid Attacks")
@@ -72,11 +70,11 @@ if az_id:
                     print("=======================   file inclusion attack blocked.   ======================")
                 else:
                     print("ERROR: Nginx App Protect dynamic page verification is Failed!!! ")
+                turn_instance_state(str(vmss_port_list[1])[-1],"start",vmssName,resource_group)
             except AssertionError:
                 print("Encountered a Problem")
                 raise
-            turn_instance_state(str(vmss_port_list[1])[-1],"start",vmssName,resource_group)
-        
+                
         if LB_TEST:
             #Load balancer Test
             print("Load Balancer TEST with Fault Tolarance")
@@ -91,33 +89,33 @@ if az_id:
             time.sleep(10)
         
         if AutoScale_TEST:
-            #Auto-scale Test
-            print("Nginx App Protect WAF - AutoScale TEST ")
-            print("Current No of instances under VMSS:",vmssName,vmss_ip_lst)
-            print("Imposing HIGH TRAFFIC on available instances")
-            for port in vmss_port_list.reverse():
-                print("Connecting to ",vmss_ip_lst[0],":",port)
-                ssh_id=ssh_connect(vmss_ip_lst[0],port,username,vm_password)
-                exec_shell_cmd(ssh_id,apply_stress)
-                #ssh_id.close()
+            try:
+                #Auto-scale Test
+                print("Nginx App Protect WAF - AutoScale TEST ")
+                print("Current No of instances under VMSS:",vmssName,vmss_ip_lst)
+                print("Imposing HIGH TRAFFIC on available instances")
+                for port in vmss_port_list.reverse():
+                    print("Connecting to ",vmss_ip_lst[0],":",port)
+                    ssh_id=ssh_connect(vmss_ip_lst[0],port,username,vm_password)
+                    exec_shell_cmd(ssh_id,apply_stress)
+                    #ssh_id.close()
 
-            print("Minimum of amount of duration to trigger the auto-scaling action: 6-7 Minutes")
-            time.sleep(500)
-            inst_info= az_get_cmd_op(get_vmss)   
-            vmss_ip_lst=get_ip(inst_info)
-            print("Number of Instances after Imposing high traffic",vmss_ip_lst) 
-            if len(get_port_lst(inst_info)) > 2:
-                print("Scaling Test is Completed Sucessfully")
-            else:
-                print("Error: Scaling Test is Failed!!!")
-            for port in vmss_port_list:
-                print("Connecting to ",vmss_ip_lst[0],":",port)
-                ssh_id=ssh_connect(vmss_ip_lst[0],port,username,vm_password)
-                exec_shell_cmd(ssh_id,remove_stress)
-                #ssh_id.close()
-        except SSHException as sshException:
-            print("Unable to establish SSH connection: %s" % sshException)
-
+                print("Minimum of amount of duration to trigger the auto-scaling action: 6-7 Minutes")
+                time.sleep(500)
+                inst_info= az_get_cmd_op(get_vmss)   
+                vmss_ip_lst=get_ip(inst_info)
+                print("Number of Instances after Imposing high traffic",vmss_ip_lst) 
+                if len(get_port_lst(inst_info)) > 2:
+                    print("Scaling Test is Completed Sucessfully")
+                else:
+                    print("Error: Scaling Test is Failed!!!")
+                for port in vmss_port_list:
+                    print("Connecting to ",vmss_ip_lst[0],":",port)
+                    ssh_id=ssh_connect(vmss_ip_lst[0],port,username,vm_password)
+                    exec_shell_cmd(ssh_id,remove_stress)
+                    #ssh_id.close()
+            except SSHException as sshException:
+                print("Unable to establish SSH connection: %s" % sshException)
 else:
     print("Error: Unable to connect to Azure CLI")
 
