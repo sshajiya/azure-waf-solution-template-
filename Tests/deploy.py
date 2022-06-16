f47d93097bc6ffd568646c9866cfa51b265590fa
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-16 21:36:07 +0530

    Update deploy.py

--- Tests/deploy.py
+++ Tests/deploy.py
@@ -14,10 +14,10 @@ if az_id:
     try:
         """If login success, then deploy resources."""
         print("AZ Login Sucessfull!!")
-        az_arm_deploy(resource_group,autoscale_template,autoscale_param)
+        #az_arm_deploy(resource_group,autoscale_template,autoscale_param)
         az_get_cmd_op(http_rule)
         az_get_cmd_op(ssh_rule)
-        az_arm_deploy(resource_group,template_db,template_dbparam,resource="db")
+        #az_arm_deploy(resource_group,template_db,template_dbparam,resource="db")
         dashboard_info=az_get_cmd_op(db_verify)
         if db_name in dashboard_info:
             print("Dashboard Created Sucessufully")
