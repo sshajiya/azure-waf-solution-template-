022e569f9090ddba35237320512cda0b3e84215f
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-20 18:24:41 +0530

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -16,25 +16,31 @@ az_id = az_login(principal,password,tenantid)
 if az_id:   
         print(text2art("CFT Testcase Execution",font="small"))
         #Get the instance details from Virtual machine scaleset
+        print(banner("+"))
         print(banner("TC-0 Infra Validation"))
+        print(banner("+"))
+        print("Get VMSS Instance details")
         inst_info=az_get_cmd_op(get_vmss)
         vmss_ip_lst=get_ip(inst_info)
         vmss_port_list=get_port_lst(inst_info)  
-        print("VMSS with Instance Details:", vmss_ip_lst, vmss_port_list) 
-        print("Dashboard : ", db_name)       
+        print("Get Telemetry Details")       
         dashboard_info=az_get_cmd_op(db_verify)
         
         if NAP_TEST:
             try:
                 #NAP Functional Test
                 turn_instance_state(str(vmss_port_list[1])[-1],"stop",vmssName,resource_group)
+                print(banner("+"))
                 print(banner("TC-1: NAP Static Page Verification"))
+                print(banner("+"))
                 if vfy_nginx(vmss_ip_lst[0],chk_def):
-                    print("NGINX Static Page Verification is Completed")
+                    print("*** NGINX Static Page Verification is Completed!!! *** \n")
                 else:
                     print("ERROR:  NGINX Static Page Verification is Failed!!!")
                 
+                print(banner("+"))                
                 print(banner("TC-2: NAP Dynamic Page Verification"))
+                print(banner("+"))
                 ssh_id=ssh_connect(vmss_ip_lst[0],vmss_port_list[0],username,vm_password)
                 with SCPClient(ssh_id.get_transport()) as scp:  scp.put('Lib/nginx_conf_nap.conf','nginx.conf')
                 for cmd in [command_lst,command_lst2]:
@@ -42,33 +48,35 @@ if az_id:
                     time.sleep(10)
                 
                 if vfy_nginx(vmss_ip_lst[0],chk_str):
-                    print("Nginx App Protect dynamic page verification with Arcadia Application is Successfull!!!")
+                    print("*** Nginx App Protect dynamic page verification with Arcadia Application is Successfull!!! *** \n")
+                    print(banner("+"))
                     print(banner("TC-3: NAP Testing with malicious attacks"))
-                    print("\t======================      cross script      ========================")
+                    print(banner("+"))                
+                    print("\n\t***      cross script      ***")
                     output = cross_script_attack(vmss_ip_lst[0])
-                    print("|\t",output)
+                    print(output)
                     assert "support ID" in output
-                    print("\t===================      cross script attack blocked. ================")
-                    print("\t======================      sql injection       ========================")
+                    print("\t***      cross script attack blocked. ***")
+                    print("\n\t***      sql injection       ***")
                     output = sql_injection_attack(vmss_ip_lst[0])
                     print(output)
                     assert "support ID" in output
-                    print("\t==================   sql injection script attack blocked.  ==================")
-                    print("\t======================      command injection       ========================")
+                    print("\t***   sql injection script attack blocked.  ***")
+                    print("\n\t***      command injection       ***")
                     output = command_injection_attack(vmss_ip_lst[0])
                     print(output)
                     assert "support ID" in output
-                    print("\t================      command injection attack blocked. =================")
-                    print("\t======================      directory traversal      ========================")
+                    print("\t***      command injection attack blocked. ***")
+                    print("\n\t***      directory traversal      ***")
                     output = directory_traversal_attack(vmss_ip_lst[0])
                     print(output)
                     assert "support ID" in output
-                    print("\t=================    directory traversal attack blocked.    ===============")
-                    print("\t======================      file inclusion      ========================")
+                    print("\t***    directory traversal attack blocked.    ***")
+                    print("\n\t***      file inclusion      ***")
                     output = file_inclusion_attack(vmss_ip_lst[0])
                     print(output)
                     assert "support ID" in output
-                    print("\t=======================   file inclusion attack blocked.   ======================")
+                    print("\t***   file inclusion attack blocked.   ***\n")
                 else:
                     print(banner("ERROR: Nginx App Protect dynamic page verification is Failed!!!"))
                 turn_instance_state(str(vmss_port_list[1])[-1],"start",vmssName,resource_group)
@@ -76,21 +84,25 @@ if az_id:
                 print("Encountered a Problem")
                 raise
                 
-        if LB_TEST:            
+        if LB_TEST:
+            print(banner("+"))
             print(banner("TC-4: Load Balancer TEST with Fault Tolarance"))
+            print(banner("+"))
             turn_instance_state(str(vmss_port_list[1])[-2],"stop",vmssName,resource_group)
             time.sleep(10)
 
             if vfy_nginx(vmss_ip_lst[0],chk_def):
-                print(banner("Load Balancer TEST with Fault Tolarance is Successfull"))
+                print(banner("\t*** Load Balancer TEST with Fault Tolarance is Successfull ***\n"))
             else:
-                print(banner("Load Balancer TEST with Fault Tolarance is Failed!!!"))
+                print(banner("\t*** Load Balancer TEST with Fault Tolarance is Failed!!! ***\n"))
             turn_instance_state(str(vmss_port_list[1])[-2],"start",vmssName,resource_group)
             time.sleep(30)
         
         if AutoScale_TEST:
             try:
+                print(banner("+"))
                 print(banner("TC-5: AutoScale TEST "))
+                print(banner("+"))
                 print("Current No of instances under VMSS:",vmssName,vmss_ip_lst,vmss_port_list)
                 print("Imposing HIGH TRAFFIC on available instances")
                 vmss_port_list.reverse()
@@ -99,13 +111,13 @@ if az_id:
                     ssh_id=ssh_connect(vmss_ip_lst[0],port,username,vm_password)
                     exec_shell_cmd(ssh_id,apply_stress)
 
-                print("Minimum of amount of duration to trigger the auto-scaling action: 6-7 Minutes")
+                print("Minimum of amount of traffic duration to trigger the auto-scaling action: 5 Minutes")
                 time.sleep(500)
-                inst_info= az_get_cmd_op(get_vmss)   
-                vmss_ip_lst=get_ip(inst_info)
-                print("Number of Instances after Imposing high traffic",vmss_ip_lst) 
+                print("Number of Instances after Imposing high traffic")                
+                inst_info= az_get_cmd_op(get_vmss)
+                vmss_ip_lst=get_ip(inst_info) 
                 if len(get_port_lst(inst_info)) > 2:
-                    print("Auto-Scaling Test is Completed Successfully")
+                    print("*** Auto-Scale Test is Completed Successfully!!! ***")
                 else:
                     print("Error: Scaling Test is Failed!!!")
                 for port in vmss_port_list:
