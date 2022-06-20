816b980226cbb8ae34411b6f2951e56f7333f4ec
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-20 15:16:13 +0530

    Update destroy.py

--- Tests/destroy.py
+++ Tests/destroy.py
@@ -15,11 +15,12 @@ az_id = az_login(principal,password,tenantid)
 if az_id:
     try:  
         print(text2art("CFT Destroy",font="small"))
-        print("Destroying the Infra.")  
+        print(banner("Destroying the Infra."))
         for service in del_cfg:
             print(service)
             az_get_cmd_op(service) 
             time.sleep(7)
+        print(banner("Infra destroyed sucessfully!!!"))
     except BaseException:
         logging.exception("An exception was thrown under Destroy!")
 else:
