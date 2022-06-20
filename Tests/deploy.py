a9fb5d9e75c85efd5eb9999f71ac5cbfa5086464
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-20 18:30:15 +0530

    Update deploy.py

--- Tests/deploy.py
+++ Tests/deploy.py
@@ -18,17 +18,16 @@ if az_id:
     try:
         """If login success, then deploy resources."""
         print("AZ Login Sucessfull!!")
-
         print(banner("Validating the user given params"))
-        validate_user_params()   
-        
+        validate_user_params()           
+        print('Deploying the Virtual Machine Scale Set')
         az_arm_deploy(resource_group,autoscale_template,autoscale_param,resource="cft")
+        print("Add the required Rules to the Security Group\n HTTP:")
         az_get_cmd_op(http_rule)
+        print("SSH:")
         az_get_cmd_op(ssh_rule)
+        print("Deploy the Telemetry")
         az_arm_deploy(resource_group,template_db,template_dbparam,resource="db")
-        #dashboard_info=az_get_cmd_op(db_verify)
-        #if db_name in dashboard_info:
-        #    print("Dashboard Created Sucessufully")
         print(banner("Deployment Completed"))
     except BaseException:
         logging.exception("An exception was thrown!")
