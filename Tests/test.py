24d53392c8e873311fe033e3252cbb8bc1b4ed11
sshajiya <39581520+sshajiya@users.noreply.github.com>
2022-06-27 19:07:55 +0530

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -14,12 +14,7 @@ password = sys.argv[2]
 print("Connecting to Azure CLI")
 az_id = az_login(principal,password,tenantid)
 if az_id:   
-        ssh_id=ssh_connect("20.115.1.72",50000,username,vm_password)
-        print("sshId: ",ssh_id)
-        if ssh_id == "retry":
-                print("Trying one more time")
         
-        '''
         print(text2art("CFT Testcase Execution",font="small"))
         #Get the instance details from Virtual machine scaleset
         print(banner("+"))
@@ -48,10 +43,11 @@ if az_id:
                 print(banner("TC-2: NAP Dynamic Page Verification"))
                 print(banner("+"))
                 ssh_id=ssh_connect(vmss_ip_lst[0],vmss_port_list[0],username,vm_password)
-                with SCPClient(ssh_id.get_transport()) as scp:  scp.put('Lib/nginx_conf_nap.conf','nginx.conf')
-                for cmd in [command_lst,command_lst2]:
-                    exec_shell_cmd(ssh_id,cmd)
-                    time.sleep(10)
+                if ssh_id != "retry":
+                    with SCPClient(ssh_id.get_transport()) as scp:  scp.put('Lib/nginx_conf_nap.conf','nginx.conf')
+                    for cmd in [command_lst,command_lst2]:
+                        exec_shell_cmd(ssh_id,cmd)
+                        time.sleep(10)
                 
                 if vfy_nginx(vmss_ip_lst[0],chk_str):
                     print("*** Nginx App Protect dynamic page verification with Arcadia Application is Successfull!!! *** \n")
@@ -106,7 +102,6 @@ if az_id:
         if AutoScale_TEST:
             try:
                 print("\n Make sure both the instances are UP")
-                turn_instance_state(str(vmss_port_list[1])[-1],"start",vmssName,resource_group)
                 turn_instance_state(str(vmss_port_list[1])[-2],"start",vmssName,resource_group)
                 time.sleep(60)
                 print(banner("+"))
@@ -118,7 +113,7 @@ if az_id:
                 for port in vmss_port_list:
                     print("Connecting to ",vmss_ip_lst[0],":",port)
                     ssh_id=ssh_connect(vmss_ip_lst[0],port,username,vm_password)
-                    exec_shell_cmd(ssh_id,apply_stress)
+                    if ssh_id != "retry": exec_shell_cmd(ssh_id,apply_stress)
 
                 print("Minimum of amount of traffic duration to trigger the auto-scaling action: 5 Minutes")
                 time.sleep(500)
@@ -134,6 +129,5 @@ if az_id:
                     exec_shell_cmd(ssh_id,remove_stress)
             except SSHException as sshException:
                 print(banner("Unable to establish SSH connection: %s" % sshException))
-        '''
 else:
     print("Error: Unable to connect to Azure CLI")
